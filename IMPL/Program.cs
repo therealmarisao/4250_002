f3246f9984f82567f0c8c0cd6e03be05d3deaced
etsuDummy <111908478+etsuDummy@users.noreply.github.com>
2023-02-27 14:12:05 -0500

    feedback provided in-line with "WCK" label
    Consider using modulus operator

--- IMPL/Program.cs
+++ IMPL/Program.cs
@@ -11,10 +11,10 @@ public class Program
     {
         int input = 0;
 
-        string inputString = Console.ReadLine();
+        string inputString = Console.ReadLine();  WCK-- This is not what the problem statement asked for. I asked to input 2 numbers and then add them together 
         //Console.WriteLine(inputString);
 
-        input = int.Parse(inputString);
+        input = int.Parse(inputString); WCK -- what happens if this fails?
         //Console.WriteLine(input);
         
         Recursion(input);
@@ -23,14 +23,14 @@ public class Program
     /*
      * Takes an int input from the parameter, then takes the last digit and adds it to the rest of the number.
      * It then calls itself again using the sum to add the last digit again. This continues until the digit is only one digit long.
-     */
+     */  WCK good comment
     public static void Recursion(int input)
     {
         Console.WriteLine(input);
-        if (input > 9)
+        if (input > 9)   WCK what if input < 0 as in -23423515
         {
 
-            string stringInput = input.ToString();
+            string stringInput = input.ToString();  WCK nice approach. I wonder if it will work for floats
 
             string lastDigitString = stringInput.Substring(stringInput.Length - 1);
             string inputWithoutLast = stringInput.Substring(0, stringInput.Length - 1);
